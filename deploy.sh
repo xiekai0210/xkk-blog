e211b61acdad682f0759c3732242abe262e8bc4f
kainiu <kainiu@syounggroup.com>
2023-04-28 17:04:09 +0800

    脚本文件

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io 
-git push -f https://github.com/xiekaixiongmao/xkk-blog.github.io.git master
+git push -f git@github.com:xiekaixiongmao/xkk-blog.git master:gh-pages
 
-cd -
+cd -      
